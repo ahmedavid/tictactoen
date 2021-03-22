49219fcd492fcfbd3991b40a9c35e516b787ea95
Samir Dadash-zada <samir.dadashzada@simbrella.com>
2021-03-22 16:19:00 +0400

    Added Java src folder - initial version

---
+++ java_src/TikTakToe/src/Action.java
@@ -0,0 +1,9 @@
+public class Action {
+    public int x;
+    public int y;
+
+    public  Action(int x, int y)
+    {
+        x = x; y = y;
+    }
+}
---
+++ java_src/TikTakToe/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game(3, (byte)1);
+    }
+}
