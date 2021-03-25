601568e38ce5720df684c0c19ec766bbc92e2640
Samir Dadash-zada <samir.dadashzada@simbrella.com>
2021-03-25 14:59:10 +0400

    Added ApiHelper for API calls; Added GSON for JSON deserialization

--- java_src/TikTakToe/src/Main.java
+++ java_src/TikTakToe/src/Main.java
@@ -1,9 +1,24 @@
-import java.util.Dictionary;
-import java.util.Hashtable;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game(3, (byte)1);
+        //Game game = new Game(3, (byte)1);
+
+        //BaseResponse response = ApiHelper.CreateGame(1243, 1246, 20, 10);
+        BaseResponse response = ApiHelper.GetMyGames();
+
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(response.ResponseBody, JsonObject.class);
+
+        JsonArray myGames = jsonObject.get("myGames").getAsJsonArray();
+        for (var game : myGames)
+        {
+            System.out.println("Game: "+game+"\n");
+        }
+
+        System.out.println(response.ResponseBody);
     }
 }
